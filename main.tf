8929043c1b71309b22e3c76291fb298dd3f35462
rahulwaykos <60727515+rahulwaykos@users.noreply.github.com>
2020-11-20 11:25:55 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,57 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "nginx" {
+  name   = "nginx_access"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx" {
+  ami                         = "ami-0dba2cb6798deb6d8"
+  subnet_id                   = var.subnet_id
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  security_groups             = [aws_security_group.nginx.id]
+  key_name                    = var.key_name
+
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+
+    connection {
+      type        = "ssh"
+      user        = var.ssh_user
+      private_key = file(var.private_key_path)
+      host        = aws_instance.nginx.public_ip
+    }
+  }
+  provisioner "var-exec" {
+    command = "ansible-playbook  -i ${aws_instance.nginx.public_ip}, --private-key ${var.private_key_path} nginx.yaml"
+  }
+}
+
+output "nginx_ip" {
+  value = aws_instance.nginx.public_ip
+}
